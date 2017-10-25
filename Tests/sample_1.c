103d2773d61cb1a7dee01cf0fc24e8acf50d1542
romain manciaux <romain.manciaux@etudiant.univ-reims.fr>
2017-10-25 22:14:59 +0200

    fix(sample_1): bad sample

--- Tests/sample_1.c
+++ Tests/sample_1.c
@@ -1,25 +1,22 @@
-#include <math.h>
 #define HEIGHT 128
 #define WIDTH 128
-int main() {
+int main() 
+{
 	int i, j;
 	int gx, gy;
 	int image[HEIGHT][WIDTH]; // Image originale en niveaux de gris.
 	int sobel[HEIGHT][WIDTH]; // Image transformée.
-	
+
+	stencil gx{1,2} = {{ 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 }};
+	stencil gy{1,2} = {{ 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 }};
+
 	// Filtre Sobel
-	for (i = 1; i < HEIGHT - 1; i++) {
-		for (j = 1; j < WIDTH - 1; j++) {
-
-			gx = 	1 * image[i-1][j-1] + 0 * image[i-1][j] - 1 * image[i-1][j+1] +
-					2 * image[i ][j-1] + 0 * image[i ][j] - 2 * image[i ][j+1] +
-					1 * image[i+1][j-1] + 0 * image[i+1][j] - 1 * image[i+1][j+1];
-
-			gy = 	1 * image[i-1][j-1] + 2 * image[i-1][j] + 1 * image[i-1][j+1] +
-					0 * image[i ][j-1] + 0 * image[i ][j] - 0 * image[i ][j+1] +
-					-1 * image[i+1][j-1] - 2 * image[i+1][j] - 1 * image[i+1][j+1];
-					
-			sobel[i][j] = sqrt((pow(gx, 2) + pow(gy, 2)) / 4.);
+	for (i = 1; i < HEIGHT - 1; i++) 
+	{
+		for (j = 1; j < WIDTH - 1; j++) 
+		{
+			sobel[i][j] = 	sqrt((pow(gx $ image[i][j], 2) +
+							pow(gy $ image[i][j], 2)) / 4.);
 		}
 	}
 	return 0;
