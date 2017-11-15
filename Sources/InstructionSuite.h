c3d7992290422b0a2d74921f50edde0277812f81
ResnahF <florian.hanser@etu.unistra.fr>
2017-11-15 19:41:43 +0100

    feat(InstructionSuite.c/h) : add prototype of InstructionSuite if you want to work without me

---
+++ Sources/InstructionSuite.h
@@ -0,0 +1,26 @@
+#ifndef __INSTRUCTIONSUITE_H
+#define __INSTRUCTIONSUITE_H
+
+#define BUFFER_SIZE 50 //on peut admettre que taille max d'une instruction est de 50
+
+/*
+
+la struct : { char * , int nbIndentation}			nbIndentation utile pour 
+
+*/
+
+
+/* Les fonction que j'aimerais bien pouvoir utilisé
+
+void addInst(...,char*,int)
+void addFirstInst(...,char*,int)
+void concatInst(... A,... B) met B a la suite de A (A->next....->next = B)
+void increInst(...,int n)	incrémente de n tout les nbIndentation de la liste
+void freeInst(...)
+
+
+
+
+*/
+
+#endif
